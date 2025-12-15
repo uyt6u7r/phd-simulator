430dac92243eddfa2810ef9b712d972c17638bf3
uyt6u7r <67103688+uyt6u7r@users.noreply.github.com>
2025-12-15 22:25:43 +1100

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './boot/App';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error("Could not find 'root' element in DOM");
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
